52ebe8822da3726939819d6805a6c10374642da5
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-09 08:54:56 -0600

    tried to fix check button

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 'use strict';
 
+$(function(){
 // the shopping list must:
 //enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
-$('form').submit(function (event) {
-  event.preventDefault();
-  let newItem = $(event.currentTarget).find('input').val();
-  $('ul').append(`<li> <span class="shopping-item"> ${newItem} </span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div> </li>`);
-}); 
+  $('form').submit(function (event) {
+    event.preventDefault();
+    let newItem = $(event.currentTarget).find('input').val();
+    $('ul').append(`<li> <span class="shopping-item"> ${newItem} </span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div> </li>`);
+  }); 
 
-//check and uncheck items on the list by clicking the "Check" button
-$('button.shopping-item-toggle').click(function () {
-  $('.shopping-item').toggleClass('.shopping-item__checked');
+  //check and uncheck items on the list by clicking the "Check" button
+  $('.shopping-list').on('click', '.shopping-item-toggle', function () {
+    $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked');
   //$('ul').append('<li> one </li>');
-});
+  });
  
-//permanently remove items from the list
-$('button.shopping-item-delete').click(function (event) {
-  $(event.currentTarget).closest('li').remove();
+  //permanently remove items from the list
+  $('button.shopping-item-delete').click(function (event) {
+    $(event.currentTarget).closest('li').remove();
+  });
 });
